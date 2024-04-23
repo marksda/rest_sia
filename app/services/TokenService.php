92160e7ab4bc0b4f948dfd5ab5270c7321b55bd7
marksda <vlack123@gmail.com>
2024-04-23 14:07:50 +0700

    cso

--- app/services/TokenService.php
+++ app/services/TokenService.php
@@ -2,6 +2,7 @@
 
 namespace MyApp\Services;
 
+use MyApp\Models\User;
 use Phalcon\Db\Column;
 use Phalcon\Encryption\Security\JWT\Builder;
 use Phalcon\Encryption\Security\JWT\Signer\Hmac;
@@ -11,65 +12,87 @@ use Phalcon\Encryption\Security\JWT\Validator;
 class TokenService extends AbstractService
 {
     public function createToken($dataCredential)
-    {        
-        $sql = "
-            SELECT 
-                id, nama, pass
-            FROM
-                public.users
-            WHERE 
-                login = :userName
-            ";
+    {   
+        
+        try {
+            $daftarUser = User::find(
+				[
+					'conditions' => '',
+					'bind'       => []
+				]
+			);
 
-        $result = $this->db->fetchOne(
-            $sql,
-            2,
-            [
-                'userName' => $dataCredential->userName
-            ],
-            [
-                Column::BIND_PARAM_STR
-            ]
-        );
+			if (!$daftarUser) {
+				return [];
+			}
 
-        $token = [];
+			$token = [];
+            foreach ($daftarUser as $user) {                
+                if($this->security->checkHash($dataCredential->password, $user->getPass()) == true) {
+                    $detail_office = $user->getRelated('detail_office_store_outlet');				
+                    $detail_office->setPropinsi($detail_office->getRelated('detail_propinsi'));
+                    $detail_office->setKabupaten($detail_office->getRelated('detail_kabupaten'));
+                    $detail_office->setKecamatan($detail_office->getRelated('detail_kecamatan'));
+                    $detail_office->setDesa($detail_office->getRelated('detail_desa'));
 
-        if($result) {
-            if($this->security->checkHash($dataCredential->password, $result['pass']) == true) {
-                // Defaults to 'sha512'
-                $signer  = new Hmac();
+                    $perusahaan = $detail_office->getRelated('detail_perusahaan');
+                    $perusahaan->setPropinsi($perusahaan->getRelated('detail_propinsi'));
+                    $perusahaan->setKabupaten($perusahaan->getRelated('detail_kabupaten'));
+                    $perusahaan->setKecamatan($perusahaan->getRelated('detail_kecamatan'));
+                    $perusahaan->setDesa($perusahaan->getRelated('detail_desa'));
 
-                // Builder object
-                $builder = new Builder($signer);
+                    $detail_office->setPerusahaan($perusahaan);
 
-                $now        = new \DateTimeImmutable();
-                $issued     = $now->getTimestamp();
-                $notBefore  = $now->modify('-1 minute')->getTimestamp();
-                $expires    = $now->modify('+1 day')->getTimestamp();
-                $passphrase = 'QcMpZ&b&mo3TPsPk668J6QH8JA$&U&m2';
+                    $hak_akses = $user->getRelated('detail_hak_akses');
+                    $modul = $hak_akses->getRelated('detail_modul');
+                    $hak_akses->setModul($modul);
 
-                // Setup
-                $builder
-                    ->setAudience('account')                        // aud
-                    ->setContentType('application/json')            // cty - header
-                    ->setExpirationTime($expires)                   // exp 
-                    ->setId($result['id'])                          // JTI id 
-                    ->setIssuedAt($issued)                          // iat 
-                    ->setIssuer('https://dlhk.ddns.net')            // iss 
-                    ->setNotBefore($notBefore)                      // nbf
-                    ->setSubject('sia')                             // sub
-                    ->setPassphrase($passphrase)                    // password 
-                ;
+                    $user->setPerusahaan($perusahaan);
+                    $user->setOffice_store_outlet($detail_office);
+                    $user->setHak_akses($hak_akses);
+                    $user->setPass(null);
 
-                $tokenObject = $builder->getToken();
+                    // Defaults to 'sha512'
+                    $signer  = new Hmac();
 
-                $token['id'] = $result['id'];
-                $token['nama'] = $result['nama'];
-                $token['token'] = $tokenObject->getToken();
-            }                
+                    // Builder object
+                    $builder = new Builder($signer);
+
+                    $now        = new \DateTimeImmutable();
+                    $issued     = $now->getTimestamp();
+                    $notBefore  = $now->modify('-1 minute')->getTimestamp();
+                    $expires    = $now->modify('+1 day')->getTimestamp();
+                    $passphrase = 'QcMpZ&b&mo3TPsPk668J6QH8JA$&U&m2';
+
+                    // Setup
+                    $builder
+                        ->setAudience('account')                        // aud
+                        ->setContentType('application/json')            // cty - header
+                        ->setExpirationTime($expires)                   // exp 
+                        ->setId($result['id'])                          // JTI id 
+                        ->setIssuedAt($issued)                          // iat 
+                        ->setIssuer('https://dlhk.ddns.net')            // iss 
+                        ->setNotBefore($notBefore)                      // nbf
+                        ->setSubject('sia')                             // sub
+                        ->setPassphrase($passphrase)                    // password 
+                    ;
+
+                    $tokenObject = $builder->getToken();
+
+                    
+                    $token['nama'] = $user->getNama();
+                    $token['office'] = $user->getOffice_store_outlet();
+                    $token['akses'] = $user->getHak_akses();
+                    $token['token'] = $tokenObject->getToken();
+                    break;
+                }
+            }
+
+			return $token; 
         }
-
-        return $token;
+        catch (PDOException $e) {
+            throw new ServiceException($e->getMessage(), $e->getCode(), $e);
+        }
     }
 
 }
