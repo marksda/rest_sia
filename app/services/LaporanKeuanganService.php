6e1d68b2985e8984613b803fdbf9b80d69221931
marksda <vlack123@gmail.com>
2024-05-04 08:00:53 +0700

    cso

--- app/services/LaporanKeuanganService.php
+++ app/services/LaporanKeuanganService.php
@@ -10,58 +10,63 @@ class LaporanKeuanganService extends AbstractService
 
     /**
 	 * Creating a new neraca saldo
+     * neraca saldo dibuat dengan cara :
+     * memindahkan saldo akhir setiap akun/rekening
+     * pada buku besar ke dalam neraca saldo 
 	 *
 	 * @param json $idPerusahaan
      * @param int $periode 
 	 */
     public function createNeracaSaldo($idPerusahaan, $periode) {
-        //sumber data berasal dari buku besar
-        //neraca saldo dibuat dengan cara memindahkan saldo akhir setiap akun pada buku besat ke dalam neraca saldo 
+        //sumber data berasal dari rekening/akun buku besar
+        createNeracaSaldoMetodeIkhtiarLabaRugi($idPerusahaan, $periode);
     }
 
     /**
-	 * Creating a new neraca saldo perusahaan dagang
-     * Metode Ikhtisar laba rugi
+	 * Creating a new neraca saldo metode Ikhtisar laba rugi
 	 *
 	 * @param json $idPerusahaan
      * @param int $periode 
 	 */
-    private function createNeracaSaldoPerusahaanDagangMetodeLabaRugi($idPerusahaan, $periode) {
+    private function createNeracaSaldoMetodeIkhtiarLabaRugi($idPerusahaan, $periode) {
         //sumber data berasal dari buku besar
         //neraca saldo dibuat dengan cara memindahkan saldo akhir setiap akun pada buku besat ke dalam neraca saldo 
     }
 
     /**
-	 * Creating a new neraca saldo perusahaan jasa
-     * Metode Ikhtisar laba rugi
-	 *
-	 * @param json $detailJurnalData
-	 */
-    private function createNeracaSaldoPerusahaanJasaMetodeLabaRugi($idPerusahaan) {
-        //sumber data berasal dari buku besar
-        //neraca saldo dibuat dengan cara memindahkan saldo akhir setiap akun pada buku besat ke dalam neraca saldo 
-    }
-
-    /**
-	 * Creating a new neraca saldo jurnal penyesuaian
+	 * Creating a new jurnal penyesuaian
 	 *
 	 * @param json $idPerusahaan
      * @param int $periode 
 	 */
     public function createJurnalPenyesuaian($idPerusahaan, $periode) {
-        //sumber data berasal dari buku besar
-        //neraca saldo dibuat dengan cara memindahkan saldo akhir setiap akun pada buku besat ke dalam neraca saldo 
+        //sumber data berasal dari neraca saldo dan data penyesuaian pada akhir priode
+        createJurnalPenyesuaianPerusahaanDagangIkhtiarLabaRugi($idPerusahaan, $periode);
     }
 
     /**
-	 * Creating a new neraca lajur / kertas kerja 
-     * Metode Ikhtisar laba rugi
+	 * Creating a new jurnal penyesuaian
+     * MetodeLabaRugi
 	 *
 	 * @param json $idPerusahaan
      * @param int $periode 
 	 */
-    private function createNeracaLajurPerusahaanDagang($idPerusahaan, $periode) {
+    private function createJurnalPenyesuaianMetodeIkhtiarLabaRugi($idPerusahaan, $periode) {
+        //sumber data berasal dari neraca saldo dan data penyesuaian pada akhir priode
+    }
+
+    /**
+	 * Creating a new neraca lajur / kertas kerja metode Ikhtisar laba rugi
+     * kertas kerja dibuat dengan cara :
+     * 1. memindahkan data sumber neraca saldo ke dalam kolom neraca saldo pada kertas kerja
+     * 2. memindahkan data sumber jurnal penyesuaian ke dalam kolom 
+	 *
+	 * @param json $idPerusahaan
+     * @param int $periode 
+	 */
+    public function createNeracaLajur($idPerusahaan, $periode) {
         //sumber data berasal dari neraca saldo dan jurnal penyesuaian
+        createNeracaLajurPerusahaanDagangMetodeIkhtiarLabaRugi($idPerusahaan, $periode);
     }
 
     /**
@@ -71,8 +76,43 @@ class LaporanKeuanganService extends AbstractService
 	 * @param json $idPerusahaan
      * @param int $periode 
 	 */
-    private function createNeracaLajurPerusahaanDagangMetodeLabaRugi($idPerusahaan, $periode) {
+    private function createNeracaLajurMetodeIkhtiarLabaRugi($idPerusahaan, $periode) {
         //sumber data berasal dari neraca saldo dan jurnal penyesuaian
+    }
+
+    /**
+	 * Creating a new laporan laba rugi
+     * 
+	 * @param json $idPerusahaan
+     * @param int $periode 
+	 */
+    public function createLaporanLabaRugi($idPerusahaan, $periode) {
+        //sumber data berasal dari neraca lajur kolom neraca saldo, laba rugi dan neraca
+        //neraca saldo dibuat dengan cara memindahkan saldo akhir setiap akun pada buku besat ke dalam neraca saldo 
+    }
+
+    /**
+	 * Creating a new laporan perubahan modal
+     * yang fitampilkan pada laporan saldo modal awal, salfo laba atau rugi, 
+     * saldo prive bila ada, saldo penambahan modal bila ada, dan saldo modal akhir
+     * 
+	 * @param json $idPerusahaan
+     * @param int $periode 
+	 */
+    public function createLaporanPerubahanModal($idPerusahaan, $periode) {
+         //sumber data berasal dari neraca lajur
+    }
+
+
+    /**
+	 * Creating a new jurnal penutup
+	 *
+	 * @param json $idPerusahaan
+     * @param int $periode 
+	 */
+    public function createJurnalPenutup($idPerusahaan, $periode) {
+        //sumber data berasal dari laporan laba rugi
+        //neraca saldo dibuat dengan cara memindahkan saldo akhir setiap akun pada buku besat ke dalam neraca saldo 
     }
 
 }
