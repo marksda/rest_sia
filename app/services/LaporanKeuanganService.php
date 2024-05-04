0bf99b23c3b081a1767ddc8012cccfb5694c0e0d
marksda <vlack123@gmail.com>
2024-05-04 09:27:20 +0700

    cso

--- app/services/LaporanKeuanganService.php
+++ app/services/LaporanKeuanganService.php
@@ -9,7 +9,10 @@ class LaporanKeuanganService extends AbstractService
 {
 
     /**
-	 * Creating a new neraca saldo
+	 * Creating a new neraca saldo.
+     * 
+     * sumber data berasal dari buku besar
+     * 
      * neraca saldo dibuat dengan cara :
      * memindahkan saldo akhir setiap akun/rekening
      * pada buku besar ke dalam neraca saldo 
@@ -34,13 +37,14 @@ class LaporanKeuanganService extends AbstractService
     }
 
     /**
-	 * Creating a new jurnal penyesuaian
+	 * Creating a new jurnal penyesuaian.
+     * 
+     * sumber data berasal dari neraca saldo dan data penyesuaian pada akhir priode
 	 *
 	 * @param json $idPerusahaan
      * @param int $periode 
 	 */
     public function createJurnalPenyesuaian($idPerusahaan, $periode) {
-        //sumber data berasal dari neraca saldo dan data penyesuaian pada akhir priode
         createJurnalPenyesuaianPerusahaanDagangIkhtiarLabaRugi($idPerusahaan, $periode);
     }
 
@@ -57,22 +61,30 @@ class LaporanKeuanganService extends AbstractService
 
     /**
 	 * Creating a new neraca lajur / kertas kerja metode Ikhtisar laba rugi
+     * sumber data berasal dari neraca saldo dan jurnal penyesuaian.
      * neraca lajur dibuat dengan cara :
      * 1. memindahkan data sumber 'neraca saldo' ke dalam 'kolom neraca saldo' pada neraca lajur
      * 2. memindahkan data sumber 'jurnal penyesuaian' ke dalam 'kolom jurnal penyesuaian' pada neraca lajur
      *    ada 2 hal yang harus diperhatikan dalam memindahkan jurnal penyesuaian kedalam neraca lajur:
-     *    1. apabila nama akun dalam jurnal penyesuaian sudah ada dalam neraca lajur maka cukup menuliskan
-     *       saldo akun debet atau kredit pada kolom jurnal penyesuaian dalam neraca lajur.
-     *    2. apabila nama akun dalam jurnal penyesuaian belum ada pada neraca lajur maka nama akun tersebut
-     *       harus dituliskan pada kolom nama akun dalam neraca lajur baru kemudian mengisi saldo akun debet
-     *       atau kredit pada kolom jurnal penyesuaian dalam neraca lajur.
-     * 3. mengisi kolom neraca saldo disesuaikan dalam neraca lajur.
+     *    1. apabila nama akun pada jurnal penyesuaian sudah ada dalam 'kolom nama akun' pada neraca lajur maka cukup menuliskan
+     *       saldo debet atau kredit dalam 'kolom jurnal penyesuaian' pada neraca lajur.
+     *    2. apabila nama akun pada jurnal penyesuaian belum ada dalam 'kolom nama akun' pada neraca lajur maka nama akun tersebut
+     *       harus dituliskan dalam 'kolom nama akun' pada neraca lajur baru kemudian mengisi saldo debet
+     *       atau kredit dalam 'kolom jurnal penyesuaian' pada neraca lajur.
+     * 3. mengisi kolom 'neraca saldo disesuaikan' dalam neraca lajur. ada 4 kondisi didalam mengisi kolom ini:
+     *    1. apabila akun hanya memiliki saldo pada 'kolom neraca saldo' maka kolom ini diisi dengan saldo yang
+     *       ada pada 'kolom neraca saldo'.
+     *    2. apabila akun memiliki saldo pada 'kolom neraca saldo' dan 'kolom jurnal penyesuaian' serta debet/kreditnya berlainan maka kolom ini diisi
+     *       dengan selisih saldo dari kedua kolom tersebut.
+     *    3. apabila akun memiliki saldo pada 'kolom neraca saldo' dan 'kolom jurnal penyesuaian' serta debet/kreditnya sama maka kolom ini diisi
+     *       dengan jumlah saldo dari kedua kolom tersebut.
+     *    4. apabila akun hanya memiliki saldo pada 'kolom jurnal penyesuaian' maka kolom ini diisi dengan saldo yang
+     *       ada pada 'kolom neraca saldo'.
 	 *
 	 * @param json $idPerusahaan
      * @param int $periode 
 	 */
     public function createNeracaLajur($idPerusahaan, $periode) {
-        //sumber data berasal dari neraca saldo dan jurnal penyesuaian
         createNeracaLajurPerusahaanDagangMetodeIkhtiarLabaRugi($idPerusahaan, $periode);
     }
 
@@ -84,7 +96,6 @@ class LaporanKeuanganService extends AbstractService
      * @param int $periode 
 	 */
     private function createNeracaLajurMetodeIkhtiarLabaRugi($idPerusahaan, $periode) {
-        //sumber data berasal dari neraca saldo dan jurnal penyesuaian
     }
 
     /**
@@ -100,7 +111,7 @@ class LaporanKeuanganService extends AbstractService
 
     /**
 	 * Creating a new laporan perubahan modal
-     * yang fitampilkan pada laporan saldo modal awal, salfo laba atau rugi, 
+     * yang ditampilkan pada laporan ini adalah saldo modal awal, saldo laba atau rugi, 
      * saldo prive bila ada, saldo penambahan modal bila ada, dan saldo modal akhir
      * 
 	 * @param json $idPerusahaan
