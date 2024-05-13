e57e6708901c1f5942c599fe4a264e4d224ceeca
marksda <vlack123@gmail.com>
2024-05-13 23:37:57 +0700

    cso

--- app/services/NeracaSaldoService.php
+++ app/services/NeracaSaldoService.php
@@ -18,163 +18,43 @@ class NeracaSaldoService extends AbstractService
     public function createNeracaSaldo($idPerusahaan, $periodeAkuntansi)
     {
 		try {
-			$this->db->begin();
-        
-            $random = new Random();
-            $jurnal = new NeracaSaldo();
+			$neracaSaldo = NeracaSaldo::findFirst(
+				[
+					'conditions' => 'tanggal = :periodeAkuntansi: AND perusahaan = :idPerusahaan:',
+					'bind'       => [
+						'periodeAkuntansi' => $periodeAkuntansi,						
+						'perusahaan' => $idPerusahaan,
+					]
+				]
+			);
 
-			//insert row to jurnal
-			$idNeracaSaldo = $random->base58(12);
-            $result = $jurnal->setId($idNeracaSaldo)
-						->setKeterangan($jurnalData->keterangan)
-						->setTanggal($jurnalData->tanggal)
-						->setJenis_jurnal($jurnalData->jenis_jurnal)
-						->setPerusahaan($jurnalData->perusahaan->id)
-						->setOffice_store_outlet($jurnalData->office_store_outlet->id)
-						->setRef_bukti($jurnalData->ref_bukti)
-						->create();
-            
-			if (!$result) {
-				$this->db->rollback();
-				throw new ServiceException('Unable to create NeracaSaldo', self::ERROR_UNABLE_CREATE_ITEM);
-			}
-
-			$keteranganPostingBukuBesar = "";
-
-			switch ($jurnalData->jenis_jurnal) {
-				case '01':
-					$keteranganPostingBukuBesar = "";
-					break;
-				case '02':
-					$keteranganPostingBukuBesar = "";
-					break;
-				case '03':
-					$keteranganPostingBukuBesar = "";
-					break;
-				case '04':
-					$keteranganPostingBukuBesar = $jurnalData->keterangan;
-					break;
-				case '05':
-					$keteranganPostingBukuBesar = $jurnalData->keterangan;
-					break;
-				case '06':
-					$keteranganPostingBukuBesar = $jurnalData->keterangan;
-					break;
-				case '07':
-					$keteranganPostingBukuBesar = $jurnalData->keterangan;
-					break;
-				case '08':
-					$keteranganPostingBukuBesar = $jurnalData->keterangan;
-					break;
-				case '09':
-					$keteranganPostingBukuBesar = $jurnalData->keterangan;
-					break;
-				case '10':
-					$keteranganPostingBukuBesar = $jurnalData->keterangan;
-					break;
-				default:
-					$keteranganPostingBukuBesar = "Posting";
-					break;
-			}
-
-			$daftarItemNeracaSaldo = $jurnalData->daftarItemNeracaSaldo;
-
-			//insert detail jurnal dan posting ke buku besar
-			foreach ($daftarItemNeracaSaldo as $itemNeracaSaldo) {
-				//insert item detail jurnal
-				$detailNeracaSaldo = new DetailNeracaSaldo();
-				$idDetailNeracaSaldo = $random->base58(12);
-				$result = $detailNeracaSaldo->setId($idDetailNeracaSaldo)
-							->setNeracaSaldo($idNeracaSaldo)
-							->setPerusahaan($itemNeracaSaldo->perusahaan->id)
-							->setAkun($itemNeracaSaldo->akun->id)
-							->setDebet_kredit($itemNeracaSaldo->debet_kredit)
-							->setNilai($itemNeracaSaldo->nilai)
-							->create();
-            
-				if (!$result) {
-					$this->db->rollback();
-					throw new ServiceException('Unable to create detailNeracaSaldo', self::ERROR_UNABLE_CREATE_ITEM);
-				}
-
-				//insert posting buku besar
+			
+			if(!$neracaSaldo) {	//neraca saldo belum ada
 				$lastSaldoAkunBukuBesar = BukuBesar::findFirst(
 					[
-						'conditions' => 'akun = :idAkun: AND perusahaan = :idPerusahaan:',
+						'conditions' => 'tanggal = :periodeAkuntansi: AND perusahaan = :idPerusahaan:',
 						'bind'       => [
-							'idAkun' => $itemNeracaSaldo->akun->id,						
-							'perusahaan' => $jurnalData->perusahaan->id,
+							'periodeAkuntansi' => $periodeAkuntansi,						
+							'perusahaan' => $idPerusahaan,
 						],
 						'order' => 'tanggal DESC'
 					]
 				);
-				
-				$bukuBesar = new BukuBesar();
 
 				if(!$lastSaldoAkunBukuBesar) {
-					$result = $bukuBesar->setId($random->base58(12))
-							->setPerusahaan($jurnalData->perusahaan->id)
-							->setTanggal($jurnalData->tanggal)
-							->setKeterangan($keteranganPostingBukuBesar)
-							->setAkun($itemNeracaSaldo->akun->id)
-							->setDebet_kredit_nilai($itemNeracaSaldo->debet_kredit)
-							->setNilai($itemNeracaSaldo->nilai)
-							->setDebet_kredit_saldo($itemNeracaSaldo->debet_kredit)
-							->setSaldo($itemNeracaSaldo->nilai)
-							->setDetail_jurnal($idDetailNeracaSaldo)
-							->setRef($jurnalData->jenis_jurnal->singkatan)
-							->create();
-					
-					if (!$result) {
-						$this->db->rollback();
-						throw new ServiceException('Unable to create detailNeracaSaldo', self::ERROR_UNABLE_CREATE_ITEM);
-					}
+					throw new ServiceException('Unable to create neraca saldo', self::ERROR_UNABLE_CREATE_ITEM);
 				}
 				else {
-					$saldoAkhir = 0.00;
-					$jenisDebetKredit = true;
 
-					if($lastSaldoAkunBukuBesar->getDebet_kredit_saldo() == $itemNeracaSaldo->debet_kredit) {
-						$jenisDebetKredit = $lastSaldoAkunBukuBesar->getDebet_kredit_saldo();
-						$saldoAkhir = $lastSaldoAkunBukuBesar->setSaldo() + $itemNeracaSaldo->nilai;
-					}
-					else {
-						$saldoAkhir = $lastSaldoAkunBukuBesar->setSaldo();
-						if($saldoAkhir >= $itemNeracaSaldo->nilai) {
-							$jenisDebetKredit = $lastSaldoAkunBukuBesar->getDebet_kredit_saldo();
-							$saldoAkhir = $saldoAkhir - $itemNeracaSaldo->nilai;
-						}
-						else {
-							$jenisDebetKredit = $itemNeracaSaldo->debet_kredit;
-							$saldoAkhir = $itemNeracaSaldo->nilai - $saldoAkhir;
-						}
-					}
-
-					$result = $bukuBesar->setId($random->base58(12))
-							->setPerusahaan($jurnalData->perusahaan->id)
-							->setTanggal($jurnalData->tanggal)
-							->setKeterangan($keteranganPostingBukuBesar)
-							->setAkun($itemNeracaSaldo->akun->id)
-							->setDebet_kredit_nilai($itemNeracaSaldo->debet_kredit)
-							->setNilai($itemNeracaSaldo->nilai)
-							->setDebet_kredit_saldo($jenisDebetKredit)
-							->setSaldo($saldoAkhir)
-							->setDetail_jurnal($idDetailNeracaSaldo)
-							->setRef($jurnalData->jenis_jurnal->singkatan)
-							->create();
-					
-					if (!$result) {
-						$this->db->rollback();
-						throw new ServiceException('Unable to create detailNeracaSaldo', self::ERROR_UNABLE_CREATE_ITEM);
-					}
-				}	
+				}
+			}
+			else {	//neraca saldo sudah ada
+				throw new ServiceException('Unable to create neraca saldo, neraca saldo periode ini sudah ada', self::ERROR_UNABLE_CREATE_ITEM);
 			}
 			
-			$this->db->commit();
         } catch (\PDOException $e) {
-			$this->db->rollback();
             if ($e->getCode() == 23505) {
-				throw new ServiceException('Item jurnal atau buku besar already exists', self::ERROR_ALREADY_EXISTS, $e);
+				throw new ServiceException('Item neraca saldo already exists', self::ERROR_ALREADY_EXISTS, $e);
 			} 
 			else if ($e->getCode() == 23503){
 				throw new ServiceException('Foreign key error', self::ERROR_FOREIGN_KEY_VIOLATION, $e);
@@ -190,13 +70,17 @@ class NeracaSaldoService extends AbstractService
 	 *
 	 * @return array
 	 */
-    public function getNeracaSaldoList($idPerusahaan, $priodeAkuntansi)
+    public function getNeracaSaldoList($idPerusahaan, $startPriodeAkuntansi, $endPriodeAkuntansi)
     {
         try {
-			$daftarNeracaSaldo = NeracaSaldo::find(
+			$lastSaldoAkunBukuBesar = BukuBesar::findFirst(
 				[
-					'conditions' => '',
-					'bind'       => []
+					'conditions' => 'tanggal = :periodeAkuntansi: AND perusahaan = :idPerusahaan:',
+					'bind'       => [
+						'periodeAkuntansi' => $periodeAkuntansi,						
+						'perusahaan' => $idPerusahaan,
+					],
+					'order' => 'tanggal DESC'
 				]
 			);
 
